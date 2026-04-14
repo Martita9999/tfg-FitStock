0d294869efc1209ba17bc298b8823e6937bff301
ILSAPINEDA <ilsapineda33@gmail.com>
2026-04-14 22:39:47 +0200

    Versión final sin errores de variables

--- FitStock/index.php
+++ FitStock/index.php
@@ -2,8 +2,8 @@
 session_start();
 require_once "conexion.php"; 
 
-$c = $_GET['c'] ?? 'Usuario';
-$a = $_GET['a'] ?? 'listar';
+$c = isset($_GET['c']) ? $_GET['c'] : 'Usuario';
+$a = isset($_GET['a']) ? $_GET['a'] : 'listar';
 
 $nombreControlador = ucfirst(strtolower($c)) . "Controller";
 $archivoControlador = "controllers/" . $nombreControlador . ".php";
@@ -11,7 +11,9 @@ $archivoControlador = "controllers/" . $nombreControlador . ".php";
 if (file_exists($archivoControlador)) {
     require_once $archivoControlador;
     $objeto = new $nombreControlador();
-    $objeto->$a();
+    if (method_exists($objeto, $a)) {
+        $objeto->$a(); // Esto llama al controlador, y el controlador carga la vista
+    }
 } else {
     header("Location: index.php?c=usuario&a=listar");
 }
